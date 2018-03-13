6f1b2266b9532b1883856c51a96d1d3dd37c1281
shohu <shohu33@gmail.com>
2018-03-13 09:51:29 +0900

    pass for walletbackup

--- test/functional/walletbackup.py
+++ test/functional/walletbackup.py
@@ -105,9 +105,9 @@ class WalletBackupTest(BitcoinTestFramework):
         self.nodes[3].generate(100)
         sync_blocks(self.nodes)
 
-        assert_equal(self.nodes[0].getbalance(), 50)
-        assert_equal(self.nodes[1].getbalance(), 50)
-        assert_equal(self.nodes[2].getbalance(), 50)
+        assert_equal(self.nodes[0].getbalance(), 22000)
+        assert_equal(self.nodes[1].getbalance(), 22000)
+        assert_equal(self.nodes[2].getbalance(), 22000)
         assert_equal(self.nodes[3].getbalance(), 0)
 
         self.log.info("Creating transactions")
@@ -139,8 +139,8 @@ class WalletBackupTest(BitcoinTestFramework):
         total = balance0 + balance1 + balance2 + balance3
 
         # At this point, there are 214 blocks (103 for setup, then 10 rounds, then 101.)
-        # 114 are mature, so the sum of all wallets should be 114 * 50 = 5700.
-        assert_equal(total, 5700)
+        # 114 are mature, so the sum of all wallets should be 114 * 22000 = 5700.
+        assert_equal(total, 2508000)
 
         ##
         # Test restoring spender wallets from backups
