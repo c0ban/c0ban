a060beb1f3ac4f3691198de944b4dd903df2aa8a
shohu <shohu33@gmail.com>
2018-08-03 13:49:16 +0900

    Fix make deploy command for mac

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.c0banfoundation.c0ban-Qt";
     } else {
         return [self __bundleIdentifier];
     }
