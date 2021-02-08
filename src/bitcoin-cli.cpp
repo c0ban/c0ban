b9cf66a51a69f6aec49040e8e0ab960ed9879406
c0ban-project <39722780+c0ban-project@users.noreply.github.com>
2021-02-08 10:23:46 +0900

    Change copyright for c0ban

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2019 The Bitcoin Core developers
-// Copyright (c) 2017-2020 The c0ban Core developers
+// Copyright (c) 2017-2021 The c0ban Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
